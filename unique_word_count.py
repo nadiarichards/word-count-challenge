c06bc142384d781ad732940d52b0bea916aa40d5
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-16 14:17:52 -0400

    unique word count completed

--- unique_word_count.py
+++ unique_word_count.py
@@ -1,2 +1,18 @@
-text = open("sample3.txt", "r")
-d = dict()
+import re
+
+sample_text = open("sample3.txt", "r")
+dict = {}
+
+for line in sample_text:
+    line=line.strip().lower()
+    line = re.sub(r'[^\w\s]','',line)
+    words=line.split(" ")
+
+    for word in words:
+        if word in dict:
+            dict[word] = dict[word]+1
+        else:
+            dict[word] =1
+
+for key in list(dict.keys()):
+    print(key, ":", dict[key])
