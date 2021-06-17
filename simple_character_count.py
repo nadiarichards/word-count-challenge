3aa4b216959b1b82d5213c6bd961cbf9eb7d2e95
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-17 14:45:24 -0400

    character count

--- simple_character_count.py
+++ simple_character_count.py
@@ -1,16 +1,19 @@
 import re
 
-sample_text = open("sample3.txt", "r")
-char_count=0
+def character_count(text_file):
+    sample_text = open(text_file, "r")
+    char_count=0
 
-for line in sample_text:
-    line=line.strip().lower()
-    line = re.sub(r'[^\w\s]','',line)
-    words=line.split(" ")
+    for line in sample_text:
+        line=line.strip().lower()
+        line = re.sub(r'[^\w\s]','',line)
+        words=line.split(" ")
 
-    for word in words:
-        word.split()
-        for character in word:
-            char_count+=1
+        for word in words:
+            word.split()
+            for character in word:
+                char_count+=1
 
-print (f"Total Characters: {char_count}")
+    print (f"Total Characters: {char_count}")
+
+character_count('sample3.txt')
