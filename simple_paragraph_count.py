eb00ebf9a18dc4c8665d2515edbb2a5acf1c56eb
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-17 14:48:31 -0400

    line and paragraph count function

--- simple_paragraph_count.py
+++ simple_paragraph_count.py
@@ -1,22 +1,22 @@
-# def insert_line_para_nums(text):
-#     text = open(text, 'r')
+def paragraph_and_line_count(text_file):
 
-sample_text = open("sample3.txt", "r")
-line_count = 0
-paragraph_count = 0
-empty = True
-for i in sample_text:
-    if '\n' in i:
-        line_count += 1
-        if len(i) < 2:
-            empty = True
-        elif len(i) > 2 and empty is True:
-            paragraph_count += 1
-            empty = False
-        if empty is True:
-            paragraph_number = 0
-    else:
-            paragraph_number = paragraph_count
-# sample_text.close()
+    sample_text = open(text_file, "r")
+    line_count = 0
+    paragraph_count = 0
+    empty = True
+    for i in sample_text:
+        if '\n' in i:
+            line_count += 1
+            if len(i) < 2:
+                empty = True
+            elif len(i) > 2 and empty is True:
+                paragraph_count += 1
+                empty = False
+            if empty is True:
+                paragraph_number = 0
+        else:
+                paragraph_number = paragraph_count
 
-print (f"Total Paragraphs: {paragraph_count} \nTotal Lines: {line_count}")
+    print (f"Total Paragraphs: {paragraph_count} \nTotal Lines: {line_count}")
+
+paragraph_and_line_count('sample3.txt')
