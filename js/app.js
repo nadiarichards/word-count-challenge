1c30112d4cb8263b31bc7de521ec1d507b698574
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-18 06:35:28 -0400

    upload text file

--- js/app.js
+++ js/app.js
@@ -25,7 +25,22 @@
 //     showTable(filteredData);
 // };
 
-function processText() {
-    let text_file=document.getElementById("txt_file")
-    console.log(text_file[0])
-}
+// function processText() {
+//     let text_file=document.getElementById("txt_file")
+//     console.log(text_file[0])
+// }
+
+function init(){
+    document.getElementById('txt_file').addEventListener('change', handleFileSelect, false);
+};
+  
+function handleFileSelect(event){
+    const reader = new FileReader()
+    reader.onload = handleFileLoad;
+    reader.readAsText(event.target.files[0])
+};
+  
+function handleFileLoad(event){
+    console.log(event);
+    document.getElementById('fileContent').textContent = event.target.result;
+};
