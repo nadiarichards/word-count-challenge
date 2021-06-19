125d13665a1894242ceaef99d7d8d882d81b38bc
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-06-19 06:26:17 -0400

    testing

--- js/app.js
+++ js/app.js
@@ -54,6 +54,30 @@ fileSelector.addEventListener('change', (event) => {
     console.log(fileList);
 });
 
+function wordCount(val) {
+    var wom = val.match(/\S+/g);
+    return {
+      charactersNoSpaces: val.replace(/\s+/g, '').length,
+      characters: val.length,
+      words: wom ? wom.length : 0,
+      lines: val.split(/\r*\n/).length
+    };
+  }
+
+var textarea = document.getElementById('textarea');
+var result = document.getElementById('result');
+  
+textarea.addEventListener('input', function() {
+    var wc = wordCount(this.value);
+    result.innerHTML = (`
+     <br>Characters (no spaces):  ${wc.charactersNoSpaces}
+     <br>Characters (and spaces): ${wc.characters}
+     <br>Words: ${wc.words}
+     <br>Lines: ${wc.lines}
+    `);
+  });
+
+
 // // Create Dropdown
 // function init() {
 
